6ba5d771f40484a281a50517deb20ebd4cda9d4c
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-03-25 16:28:25 -0300

    Finalizada tela de cadastro e roteamento da mesma.

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -2,8 +2,41 @@ import React from 'react';
 
 import './styles.css';
 
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg'
+
 export default function Register() {
   return (
-    <h1>Register</h1>
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="" />
+
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Já possuo cadastro
+          </Link>
+        </section>
+
+        <form>
+          <input type="text" placeholder="Nome da ONG" />
+          <input type="email" placeholder="Email" />
+          <input placeholder="Whatsapp" />
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
   )
 }
