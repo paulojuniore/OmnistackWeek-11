7695d0e75d721517928abc714872c5f53cc1349d
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-03-26 14:01:39 -0300

    Fazendo login e renderizando casos de ONGS atráves da api.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
+
+import api from '../../services/api';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 import { FiLogIn } from 'react-icons/fi';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogon(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+      localStorage.setItem('ongid', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login, tente novamente.')
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="logo" />
 
-        <form>
+        <form onSubmit={handleLogon}>
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID" />
+          <input 
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)} />
+
           <button className="button" type="submit">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
